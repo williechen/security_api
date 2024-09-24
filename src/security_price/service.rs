a58ead2f9f11b606c15d7d1850b789922234cb20
Chen Willie Guan-yu <bcskcnuj@hotmail.com>
2024-09-24 22:01:04 +0800

    微修

--- src/security_price/service.rs
+++ src/security_price/service.rs
@@ -22,9 +22,8 @@ pub fn get_security_to_price(task: &DailyTask) -> Result<(), SecurityError> {
 
     let q_year = task.open_date_year.clone();
     let q_month = task.open_date_month.clone();
-    let q_day = task.open_date_day.clone();
 
-    let res_prices = dao::read_all_by_res(q_year, q_month, q_day);
+    let res_prices = dao::read_all_by_res(q_year, q_month);
     for price in res_prices {
         debug!(target: "security_api", "ResposePrice: {:?}", &price);
 
